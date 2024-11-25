7d3aec05d4a9aa7b8241c0492a2bf143f2d5f556
berry <raspberry@school.com>
2024-11-25 10:04:07 +0300

    bug fixes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from model import Camera, StereoCalibrationParams
 from camera.stereo import StereoCamera
 from camera.utils import Utils
+from camera.video_stream import VideoStream
 from detector.Detector import Detector
+import numpy as np
 import cv2 as cv
 
 utils = Utils()
@@ -14,6 +16,7 @@ left_cam = Camera(
     cv_index=0,
     framerate=30
 )
+
 
 right_cam = Camera(
     cv.VideoCapture(2),
@@ -31,8 +34,8 @@ stereo_camera = StereoCamera(
         focal_length=None,
         baseline=87,
         block_size=4,
-        num_disparities=16 * 7,
-        min_disparity=0,
+        num_disparities=16 * 2,
+        min_disparity=8 ,
         disp12MaxDiff=10,
         uniquenessRatio=15,
         speckle_window_size=50,
@@ -46,9 +49,11 @@ stereo_camera = StereoCamera(
 while True:
     res, Limg = left_cam.capture.read()
     res, Rimg = right_cam.capture.read()
-    # cv.imshow("Left Image", Limg)
+    cv.imshow("Left Image", Limg)
     # cv.imshow("Right Image", Rimg)
-    stereo_camera.Test(Limg, Rimg)
+    disparity = stereo_camera.Test(Limg, Rimg)
+    cv.imshow("Disparity", cv.normalize(disparity, None, 0, 255, cv.NORM_MINMAX).astype(np.uint8))
+
     if cv.waitKey(1) & 0xFF == ord("q"):
         break
 cv.destroyAllWindows()
