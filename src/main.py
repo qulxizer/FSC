c0d949a18ef328940cd5baf4e30f2f3c45db01be
mahdi hussain <qulx@1jz.cc>
2024-11-25 21:57:05 +0300

    apparently wait key is required

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ left_cam = Camera(
     utils.loadCalibrationResultFrom("dataset/opencv_sample/left/calibration.npz"),
     "Left Camera",
     cv_index=0,
-    framerate=30
+    framerate=30,
 )
 
 
@@ -24,19 +24,19 @@ right_cam = Camera(
     utils.loadCalibrationResultFrom("dataset/opencv_sample/right/calibration.npz"),
     "Right Camera",
     cv_index=2,
-    framerate=30
+    framerate=30,
 )
 
 
 stereo_camera = StereoCamera(
-    left_camera=left_cam, 
+    left_camera=left_cam,
     right_camera=right_cam,
     params=StereoCalibrationParams(
         focal_length=None,
         baseline=87,
         block_size=4,
         num_disparities=16 * 2,
-        min_disparity=8 ,
+        min_disparity=8,
         disp12MaxDiff=10,
         uniquenessRatio=15,
         speckle_window_size=50,
@@ -46,17 +46,18 @@ stereo_camera = StereoCamera(
 
 Limg = cv.imread("/home/qulx/Dev/FSC/dataset/opencv_sample/left/left.png")
 Rimg = cv.imread("/home/qulx/Dev/FSC/dataset/opencv_sample/right/right.png")
-
 # res, Limg = left_cam.capture.read()
 # res, Rimg = right_cam.capture.read()
-cv.imshow("Left Image", Limg)
-time.sleep(100)
-
+# cv.imshow("Left Image", Limg)
+# cv.waitKey(1)
+# time.sleep(100)
 # cv.imshow("Right Image", Rimg)
 disparity = stereo_camera.Test(Limg, Rimg)
-cv.imshow("Disparity", cv.normalize(disparity, None, 0, 255, cv.NORM_MINMAX).astype(np.uint8)) # type: ignore
-time.sleep(100)
-# detector = Detector("/home/qulx/Dev/FSC/models/best.pt")
+cv.imshow(
+    "Disparity", cv.normalize(disparity, None, 0, 255, cv.NORM_MINMAX).astype(np.uint8)
+)  # type: ignore
+cv.waitKey(1)
+
 # img = cv.imread("/home/qulx/Dev/FSC/tmp/20241124_205817.jpg")
 # img = cv.resize(img, (500,1000))
 # detector.detect(img)
