bcceae9b73b43e4418da803248cf21cc7a652d92
mahdi hussain <qulx@1jz.cc>
2024-12-20 17:27:15 +0300

    restructioring python code

--- src/detection/main.py
+++ src/detection/main.py
@@ -36,25 +36,62 @@ def writeDetection(name, detection):
 
 
 def writeJsonToSharedMemory(name, data: list):
+    # Dumping the list into json object and encoding it to utf-8
     json_data = json.dumps(data)
-    print(json_data)
     json_bytes = json_data.encode("utf-8")
 
+    shm = None
     try:
-        # Try to open shared memory, it will raise FileExistsError if it already exists
+        # Creating shared_memory with buffer size of 1 megabyte
+        shm = shared_memory.SharedMemory(name=name, create=True, size=1000)
+        print("Shared memory created.")
+
+    # If there is already shared_memory instance with the same name we use it
+    except FileExistsError:
         shm = shared_memory.SharedMemory(name=name)
-        print(f"Shared memory with name {name} already exists. Overwriting...")
-        # Unlink the existing shared memory before overwriting
-        shm.unlink()
-    except FileNotFoundError:
-        # Shared memory does not exist, no need to unlink
-        pass
-    
-    # Create shared memory with the correct size
-    shm = shared_memory.SharedMemory(name=name, create=True, size=len(json_bytes))
+        print("Shared memory already exists.")
+
     # Write the JSON bytes to the shared memory buffer
-    np.copyto(np.ndarray(len(json_bytes), dtype=np.uint8, buffer=shm.buf), np.frombuffer(json_bytes, dtype=np.uint8))
-    print(f"JSON data written to shared memory {name}.")
+    if shm is not None:
+        np.copyto(
+            np.ndarray(len(json_bytes), dtype=np.uint8, buffer=shm.buf),
+            np.frombuffer(json_bytes, dtype=np.uint8),
+        )
+        print(f"JSON data written to shared memory {name}.")
+
+
+def parseResultsIntoJson(results_list):
+    # For extra information about what is happenning here i recommend reading the docs for ultralytics
+    detectionsJson = []
+    for results in results_list:
+        if (
+            results.boxes is not None and results.boxes.xyxy.shape[0] > 0
+        ):  # Check if there are any detections
+            for box in results.boxes:
+                b = box.xyxy  # get box coordinates in (left, top, right, bottom) format
+                c = box.cls
+                bboxs = b.tolist()
+                detections = []
+                for bbox, className in zip(bboxs, c):
+                    xCenter = (bbox[0] + bbox[2]) / 2  # type: ignore
+                    yCenter = (bbox[1] + bbox[3]) / 2  # type: ignore
+                    detections.append([xCenter, yCenter, className.item()])
+
+                for detection in detections:
+                    xCenter = detection[0]
+                    yCenter = detection[1]
+                    detectionClass = ClassNames(detection[2])
+                    detectionJson = {
+                        "Class": detectionClass.name,
+                        "xCenter": xCenter,
+                        "yCenter": yCenter,
+                    }
+                    detectionsJson.append(detectionJson)
+
+    if len(detectionsJson) < 1:
+        detectionsJson = {"message": "no detections were found"}
+
+    return detectionsJson
 
 
 def listener(shm_name: str):
@@ -69,51 +106,14 @@ def listener(shm_name: str):
     while True:
         try:
             image, shm = readSharedCvmat(shm_name, imageShape, imageDtype)
-            detectionsJson = []
 
             try:
                 # Perform model inference
                 cv2.waitKey(1)  # Display the image (non-blocking wait)
                 results_list = model.predict(source=image[:, :, :3], show=True)
-
                 # Process each `Results` object in the `results_list`
-                for results in results_list:
-                    if (
-                        results.boxes is not None and results.boxes.xyxy.shape[0] > 0
-                    ):  # Check if there are any detections
-                        for box in results.boxes:
-                            b = box.xyxy  # get box coordinates in (left, top, right, bottom) format
-                            c = box.cls
-                            bboxs = b.tolist()
-                            detections = []
-                            for bbox, className in zip(bboxs, c):
-                                xCenter = (bbox[0] + bbox[2]) / 2 # type: ignore
-                                yCenter = (bbox[1] + bbox[3]) / 2 # type: ignore
-                                detections.append([xCenter, yCenter, className.item()])
-
-                            for detection in detections:
-                                xCenter = detection[0]
-                                yCenter = detection[1]
-                                detectionClass = ClassNames(detection[2])
-                                detectionJson = {
-                                    "Class": detectionClass.name,
-                                    "xCenter": xCenter,
-                                    "yCenter": yCenter,
-                                }
-                                detectionsJson.append(detectionJson)
-                                print(
-                                    f"xCenter: {xCenter}, yCenter: {yCenter}, , Class: {detectionClass.name}"
-                                )
-                            writeJsonToSharedMemory("2d_coordinates", detectionsJson)
-                
-                # if no detection were found
-                
-                if len(detectionsJson) < 1:
-                    detectionsJson = {
-                        "message": "no detections were found"
-                    }
-                    writeJsonToSharedMemory("2d_coordinates", detectionsJson) # type: ignore
-
+                detectionsJson = parseResultsIntoJson(results_list)
+                writeJsonToSharedMemory("2d_coordinates", detectionsJson)  # type: ignore
 
             finally:
                 shm.unlink()  # Free the shared memory
@@ -128,6 +128,7 @@ def listener(shm_name: str):
             pass
         except Exception as e:
             print(f"{type(e)}: {e}")
+            continue
 
 
 if __name__ == "__main__":
